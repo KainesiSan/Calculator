faee32d1f135a4aaa0e8b15adaf6f79adb79cafc
KainesiSan <102213825+KainesiSan@users.noreply.github.com>
2022-03-24 21:32:25 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CSharp_Shell
             double firstNumber = 0;
             
             /* Оператор */
-            char sign = 'a';
+            char sign = ' ';
             
             /* Второе число */
             double secondNumber = 0;
@@ -27,44 +27,55 @@ namespace CSharp_Shell
                 {
                     Console.WriteLine("Введите оператор (+, -, *, /, ^, √): ");
                     sign = Convert.ToChar(Console.ReadLine());
+                    
+                    if (sign == '+' || sign == '-' || sign == '*' || sign == '/' || sign == '^' || sign == '√')
+                    {
+                        try
+                        {
+                            Console.WriteLine("Введите второе число: ");
+                            secondNumber = Convert.ToDouble(Console.ReadLine());
+                            
+                            switch (sign)
+                            {
+                                case '+':
+                                    result = firstNumber + secondNumber;
+                                    Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
+                                    break;
+                                case '-':
+                                    result = firstNumber - secondNumber;
+                                    Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
+                                    break;
+                                case '*':
+                                    result = firstNumber * secondNumber;
+                                    Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
+                                    break;
+                                case '/':
+                                    result = firstNumber / secondNumber;
+                                    Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
+                                    break;
+                                case '^':
+                                    result = Math.Pow(firstNumber, secondNumber);
+                                    Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
+                                    break;
+                                case '√':
+                                    result = Math.Pow(secondNumber, 1 / firstNumber);
+                                    Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
+                                    break;
+                            }
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Число не может содержать буквы или спецсимволы.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Такого оператора не существует.");
+                    }
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine("Такого оператора не существует.");
-                }
-                
-                Console.WriteLine("Введите второе число: ");
-                secondNumber = Convert.ToDouble(Console.ReadLine());
-                
-                switch (sign)
-                {
-                    case '+':
-                        result = firstNumber + secondNumber;
-                        Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
-                        break;
-                    case '-':
-                        result = firstNumber - secondNumber;
-                        Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
-                        break;
-                    case '*':
-                        result = firstNumber * secondNumber;
-                        Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
-                        break;
-                    case '/':
-                        result = firstNumber / secondNumber;
-                        Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
-                        break;
-                    case '^':
-                        result = Math.Pow(firstNumber, secondNumber);
-                        Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
-                        break;
-                    case '√':
-                        result = Math.Pow(secondNumber, 1 / firstNumber);
-                        Console.Write($"{firstNumber}{sign}{secondNumber} = {result}");
-                        break;
-                    default:
-                        Console.WriteLine("Такого оператора не существует.");
-                        break;
                 }
             }
             catch (FormatException)
